f2574535579ceb1b3dd929a3d2772b146d690088
6666 <system9@163.com>
2018-04-17 18:32:56 +0800

    添加WX端请求接口模块

---
+++ src/com/suyin/decorate/DecorateController.java
@@ -0,0 +1,17 @@
+package com.suyin.decorate;
+
+import com.suyin.common.service.ModuleNameService;
+import com.suyin.utils.HttpClientUtils;
+import com.suyin.utils.Utils;
+
+/**
+ * 装修活动接口请求类
+ * @author Administrator
+ *
+ */
+public class DecorateController {
+
+	//	    String result=HttpClientUtils.getRemote("/activity/numberplus").toString();
+	//      String result=HttpClientUtils.postRemote("/actrecord/joinAct",  Utils.convert(request, ModuleNameService.EXP),null).toString();
+
+}
